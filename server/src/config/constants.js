d4c6a02c6b691482eddc07976b62e50c1ccdff33
youngjoo <urdlove@gmail.com>
2017-10-24 22:16:04 +0900

    Part 01
    - graphql setup with mock data

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,4 +1,5 @@
 export default {
   PORT: process.env.PORT || 3000,
   DB_URL: 'mongodb://localhost/twitter-dev',
+  GRAPHQL_URL: '/graphql',
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
 
 import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
+import mocks from './mocks';
 
 const server = express();
 
-const PORT = process.env.PORT || 3000;
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 server.use(bodyParser.json());
 
-server.listen(PORT, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`App listen to port: ${PORT}`);
-  }
+server.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_URL,
+  })
+);
+
+server.use(
+  constants.GRAPHQL_URL,
+  graphqlExpress({
+    schema,
+  })
+);
+
+const graphQLServer = createServer(server);
+
+mocks().then(() => {
+  graphQLServer.listen(constants.PORT, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`App listen to port: ${constants.PORT}`);
+    }
+  });
 });
